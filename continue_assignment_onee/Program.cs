ac598fb407920842f33809b418de4db2c753dcba
NadaFayez13 <naditafayez13@gmail.com>
2026-09-01 11:23:00 +0300

    Question 14

--- continue_assignment_onee/Program.cs
+++ continue_assignment_onee/Program.cs
@@ -66,6 +66,18 @@ namespace continue_assignment_onee
             Book? book = null;
             Console.WriteLine(book?.Title);
             #endregion
+
+            #region Question 14
+            /* Using title from the previous question, use ?? to print "Untitled" if title is null.
+            Then use ??=  to assign title the value "Untitled" only if it's still null. */
+            string? title = book?.Title;
+            Console.WriteLine(title ?? "Untitled");
+            Console.WriteLine(title==null);
+
+            title ??= "Untitled";
+            Console.WriteLine(title);
+
+            #endregion
         }
     }
 }
