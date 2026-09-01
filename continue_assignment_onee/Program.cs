29946a036a8fcd5d07ef6bcb89323caa0a4db9ec
NadaFayez13 <naditafayez13@gmail.com>
2026-09-01 08:33:42 +0300

    Question 8

--- continue_assignment_onee/Program.cs
+++ continue_assignment_onee/Program.cs
@@ -10,6 +10,21 @@
             int pages = Convert.ToInt32(pagesText);
             Console.WriteLine(pages);
             #endregion
+
+            #region Question 8
+            /*Given string yearText = "2023";, convert it using int.Parse(). Then given string badText = "abc";
+            use int.TryParse() to safely try converting it, and print "Invalid number" if it fails */
+            string yearText = "2023";
+            int year = int.Parse(yearText);
+            Console.WriteLine(year);
+
+            string badText = "abc";
+            bool parsed = int.TryParse(badText, out int bad);
+            if (!parsed)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            #endregion
         }
     }
 }
